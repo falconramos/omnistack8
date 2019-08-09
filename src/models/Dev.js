66b199fafe01ee47b388d682c806fa4f54cf0e88
Daniel de Jesus Antunes Martins Ramos <daniel.ramos@etapa.com.br>
2019-08-09 16:26:47 -0300

    lesson 1 complete

--- src/models/Dev.js
+++ src/models/Dev.js
@@ -12,7 +12,15 @@ const DevSchema = new Schema({
     avatar: {
         type: String,
         require: true,
-    }
+    },
+    likes: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Dev', 
+    }],
+    dislikes: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Dev', 
+    }],
 },{
     timestamps: true,
 });
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose  = require('mongoose');
+const cors  = require('cors');
 
 const routes = require('./routes');
 
@@ -8,6 +9,8 @@ const server = express();
 mongoose.connect('mongodb+srv://omnistack:Etapa654@cluster0-hnjd3.mongodb.net/omnistack8?retryWrites=true&w=majority',{
     useNewUrlParser: true
 });
+
+server.use(cors());
 
 server.use(express.json());
 
