4ad6d03e9799fc560d386f57de75b5af8513330d
Daniel de Jesus Antunes Martins Ramos <daniel.ramos@etapa.com.br>
2019-08-09 15:40:51 -0300

    remove unnecessary comments

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 module.exports = {
     async store(req, res) {
-        // console.log(req.body.username);
+
         const { username } = req.body;
 
         const response = await axios.get(`https://api.github.com/users/${username}`);
--- src/server.js
+++ src/server.js
@@ -9,14 +9,6 @@ mongoose.connect('mongodb+srv://omnistack:Etapa654@cluster0-hnjd3.mongodb.net/om
     useNewUrlParser: true
 });
 
-// indentico a server.get('/', function(req, res){
-// });
-// server.get('/', (req, res) =>{
-//     // return res.send('Olá Mundo!');
-//     // return res.send(`Olá ${req.query.name}!`);
-//     return res.json({ message: `Olá ${req.query.name}` });
-// });
-
 server.use(express.json());
 
 server.use(routes);
